d4d9a0af5e6f51b32f8f8f1823384416f7ca2167
Iain Attwater <ia@dbi-nyc.com>
2024-08-28 20:42:36 -0400

    Middleware updates to correct issue with logged in user

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import NextAuth from "next-auth"
+import { NextResponse } from 'next/server'
 
 import authConfig from "@/auth.config"
 import {
@@ -19,28 +20,21 @@ export default auth((req) => {
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
 
   if (isApiAuthRoute) {
-    return
+    return null
   }
 
   if (isAuthRoute) {
     if (isLoggedIn) {
-      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+      return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
     }
-    return
+    return null
   }
-  
+
   if (!isLoggedIn && !isPublicRoute) {
-    let callbackUrl = nextUrl.pathname
-    if (nextUrl.search) {
-      callbackUrl += nextUrl.search
-    }
-
-    const encondedCallbackUrl = encodeURIComponent(callbackUrl) 
-
-    return Response.redirect(new URL(`/auth/login?callbackUrl=${encondedCallbackUrl}`, nextUrl))
+    return NextResponse.redirect(new URL('/auth/login', nextUrl))
   }
 
-  return
+  return null
 })
  
 // Optionally, don't invoke Middleware on some paths
